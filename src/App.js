a604848464a4bcf06fd557c8053699cdce7bbc28
KrystianB <krystiantsp@gmail.com>
2023-08-01 23:31:14 +0200

    Praca nad panelem uzytkownika

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Routes, Switch, Route } from 'react-router-dom';
 import LandingPage from "./pages/LandingPage";
 import CheckoutPage from './pages/CheckoutPage';
+import LoginPage from './pages/LoginPage';
+import DashboardPage from './pages/DashboardPage';
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
         {/* <Route path="/" exact element={<Notes />} /> */}
         <Route path="/korepetycje/:dynamicWord" element={<LandingPage />} />
         <Route path="/koszyk/" element={<CheckoutPage />} />
+        <Route path="/logowanie/" element={<LoginPage />} />
+        <Route path="/panel_uzytkownika/" element={<DashboardPage />} />
       </Routes>
     </Router>
   );
---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import HeadLine from "../components/HeadLine";
+import Signin from "../components/Login/SignIn"
+import "./LoginPage.css"
+
+
+const LoginPage = () => {
+
+    return (
+        <div>
+            <HeadLine />
+            <Signin />
+        </div>
+    )
+}
+
+export default LoginPage
